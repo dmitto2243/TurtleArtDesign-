00654bcf43bc50fcd7b73caa32ff853fe93bd9b1
dmitto2243 <33318928+dmitto2243@users.noreply.github.com>
2017-11-02 08:28:51 -0400

    Add files via upload

---
+++ project.py
@@ -0,0 +1,36 @@
+import turtle 
+turtle.bgcolor("black")
+bob = turtle.Turtle()
+turtle.tracer(0)
+
+bob.speed(10)
+bob.color("red")
+for times in range(180):
+    bob.forward(100)
+    bob.right(30)
+    bob.forward(20)
+    bob.left(60)
+    bob.forward(50)
+    bob.right(30)
+    
+    bob.penup()
+    bob.setposition(0, 0)
+    bob.pendown()
+    
+    bob.right(2)
+    
+
+for times in range(256):
+    bob.circle(10000)
+    bob.right(120)
+    bob.left(180)
+    bob.right(150)
+    bob.left(130)
+    bob.right(110)
+    bob.left(100)
+    bob.right(20)
+    bob.left(80)
+    bob.right(12)
+    bob.left(18)
+    bob.right(2)
+    bob.left(8)
